3ad21c511ba8a1efb227fd7e6e1f154919612aa7
syed-hamza-ali-hamdani <syedhamzaalihamdani@gmail.com>
2025-01-17 14:19:31 +0500

    first commit

---
+++ sanity-shecma/order.js
@@ -0,0 +1,11 @@
+{
+    "name": "order",
+    "type": "document",
+    "fields":
+    { "name": "buyer", "type": "reference", "to": [{ "type": "user" }] },
+    { "name": "items", "type": "array", "of": [{ "type": "reference", "to": [{ "type": "product" }] }] },
+    { "name": "total", "type": "number" },
+    { "name": "status", "type": "string", "options": ["pending", "shipped", "delivered", "cancelled"]
+   }
+    ]
+   }
---
+++ sanity-shecma/product.js
@@ -0,0 +1,12 @@
+{
+    "name": "product",
+    "type": "document",
+    "fields":
+    { "name": "title", "type": "string" },
+    { "name": "description", "type": "text" },
+    { "name": "price", "type": "number" },
+    { "name": "category", "type": "reference", "to": [{ "type": "category" }] },
+    { "name": "images", "type": "array", "of": [{ "type": "image" }] },
+    { "name": "stock", "type": "number" }
+    ]
+   }
